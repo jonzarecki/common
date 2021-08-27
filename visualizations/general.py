dc76f4d34f417ddebae347a89ef96998f49669d5
Jonathan Zarecki <yonilx@gmail.com>
2021-08-27 16:24:04 +0300

    feat: move multi-line plot function to common
    might be useful somewhere else

--- visualizations/general.py
+++ visualizations/general.py
@@ -1,14 +1,12 @@
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
+import holoviews as hv
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from matplotlib.axes import Axes
-from matplotlib.figure import Figure
 from PIL import Image
-from skimage import img_as_ubyte
-from torch.utils.data import Dataset
+from matplotlib.figure import Figure
 
 
 def plot_confusion_matrix(
@@ -68,51 +66,20 @@ def show_ndarray_in_matplotlib(img: np.ndarray):
     img.show()
 
 
-def plot_examples_from_dataset(ds: Dataset, idxs: List[int], title: str = "", fig: Figure = None) -> Optional[Figure]:
-    """Plot images from the dataset according to $idxs.
+def plot_hv_multi_line_curve(curves: List[List[float]], title: str) -> hv.Curve:
+    """Plots a multi line curve with holoviews.
 
     Args:
-        ds: torch image Dataset
-        idxs: Indices that we want to plot
-        title: Title for the plot
-        fig: An optional figure to plot on
+        curves:  List of lists (of the same length!) with points on the curves
+        title: Plot title
 
     Returns:
-        Figure is a figure was passed, else it plots the images as described.
+        a holoviews curve object
     """
-    image_datas = [ds[i][0].numpy().swapaxes(0, 2).swapaxes(0, 1) for i in idxs]
-    if image_datas[0].dtype == np.float32:
-        for i, img in enumerate(image_datas):
-            im_max, im_min = max(1.0, img.max()), min(0.0, img.min())
-            img = (img - im_min) / (im_max - im_min)
-            image_datas[i] = img_as_ubyte(img)
-    image_labels = [ds.classes[ds[i][1]] for i in idxs]
+    assert all(len(curves[0]) == len(curv) for curv in curves), "all curves should be of the same length " \
+                                                                "(have the same x axis)"
+    curv_plt = hv.Curve(curves[0])
+    for curv in curves[1:]:
+        curv_plt *= hv.Curve(curv)
 
-    fig_avail = True
-    if fig is None:
-        fig_avail = False
-        fig = plt.figure()
-    axes: List[List[Axes]] = fig.subplots(
-        len(idxs) // 3 + (1 if len(idxs) % 3 != 0 else 0), min(len(idxs), 3), squeeze=False
-    )
-
-    fig.suptitle(title)
-
-    if len(idxs) == 1:
-        axes[0][0].imshow(image_datas[0])
-        axes[0][0].axis("off")
-        (_width, height) = fig.get_size_inches()
-        fig.set_size_inches(height, height)
-
-    for j, idx in enumerate(idxs):
-        axes[j // 3][j % 3].imshow(image_datas[j])
-        axes[j // 3][j % 3].axis("off")
-        axes[j // 3][j % 3].set_title(f"{idx} - {image_labels[j]}")
-    fig.tight_layout()
-    plt.tight_layout()
-    if fig_avail:
-        return fig
-    else:
-        plt.show()
-        return None
-    # TODO: add functionality from matplotlib to bokeh with image in between
+    return curv_plt.opts(fontscale=1, width=500, height=400, title=title, toolbar='above')
